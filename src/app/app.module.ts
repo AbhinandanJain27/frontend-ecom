78bae6db3c74e6a99f24cf716f8eff966160319e
Jain <abhinandan.jain@worldline.com>
2024-10-11 12:03:56 +0530

    Auth Interceptor Added in frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { JwtHelperService, JWT_OPTIONS } from '@auth0/angular-jwt';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
+import { AuthInterceptor } from './auth/auth.interceptor';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient,HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NotAuthorizedComponent } from './auth/not-authorized/not-authorized.component';
 
 @NgModule({
@@ -29,6 +30,7 @@ import { NotAuthorizedComponent } from './auth/not-authorized/not-authorized.com
     provideClientHydration(),
     provideHttpClient(),
     {provide: JWT_OPTIONS, useValue: JWT_OPTIONS},
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
     JwtHelperService
   ],
   bootstrap: [AppComponent]
